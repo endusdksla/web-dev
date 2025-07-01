72d56bcd7b7e23e370576d0df3d130cd87c89dfa
revivalroot <117237980+endusdksla@users.noreply.github.com>
2025-07-02 03:58:42 +0900

    Update delete_ok.php

--- delete_ok.php
+++ delete_ok.php
@@ -5,14 +5,14 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 include 'db_connect.php';
 
-// [변경 1] 어떤 게시판인지 식별자를 받아옵니다.
+// 어떤 게시판인지 식별자를 받아옵니다.
 $board_id = isset($_GET['board']) ? $_GET['board'] : 'free';
 $allowed_boards = ['free', 'guestbook'];
 if (!in_array($board_id, $allowed_boards)) {
     die("존재하지 않는 게시판입니다.");
 }
 
-// [변경 2] 식별자를 이용해 테이블 이름을 동적으로 만듭니다.
+// 식별자를 이용해 테이블 이름을 동적으로 만듭니다.
 $board_table = "board_" . $board_id;
 
 $no = $_GET['id'];
