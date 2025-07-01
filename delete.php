1e813be8f43921f974d45f7480dce26a11d133a0
revivalroot <117237980+endusdksla@users.noreply.github.com>
2025-07-02 03:58:21 +0900

    Update delete.php

--- delete.php
+++ delete.php
@@ -5,19 +5,19 @@ include 'db_connect.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// [변경 1] 어떤 게시판인지 식별자를 받아옵니다.
+// 어떤 게시판인지 식별자를 받아옵니다.
 $board_id = isset($_GET['board']) ? $_GET['board'] : 'free';
 $allowed_boards = ['free', 'guestbook'];
 if (!in_array($board_id, $allowed_boards)) {
     die("존재하지 않는 게시판입니다.");
 }
 
-// [변경 2] 식별자를 이용해 테이블 이름을 동적으로 만듭니다.
+// 식별자를 이용해 테이블 이름을 동적으로 만듭니다.
 $board_table = "board_" . $board_id;
 
 $no = $_GET['id'];
 
-// [변경 3] 쿼리에서 테이블 이름을 변수로 변경합니다.
+// 쿼리에서 테이블 이름을 변수로 변경합니다.
 $query = "select * from $board_table where id=$no";
 $result = mysqli_query($conn, $query);
 $board = mysqli_fetch_array($result);
